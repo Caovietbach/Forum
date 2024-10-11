3867ffd577d0e35fefef0fea7c84ab767a3dc7c4
Caovietbach <46390324+Caovietbach@users.noreply.github.com>
2024-10-11 16:39:10 +0700

    Fixing login and add register function

--- src/main/java/org/example/forum/configuration/security/SecurityConfig.java
+++ src/main/java/org/example/forum/configuration/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/logout","/register").permitAll()
+                        .requestMatchers("/forum").hasAnyRole("USER","ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class)
